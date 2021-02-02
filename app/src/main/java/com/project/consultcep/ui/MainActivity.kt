032f477b6761f96a4f596e9e77a3e78fb64e3363
Eric Roberto <58601124+YokoCross@users.noreply.github.com>
2021-02-02 17:34:22 -0300

    layout Home added

--- app/src/main/java/com/project/consultcep/ui/MainActivity.kt
+++ app/src/main/java/com/project/consultcep/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.project.consultcep.ui
 
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -14,6 +15,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        actionBar?.hide()
 
     }
 }
