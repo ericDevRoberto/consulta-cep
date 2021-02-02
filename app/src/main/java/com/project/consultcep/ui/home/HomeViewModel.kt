6031f0ae133b5ef3579442667ad55af1cdf1a403
Eric Roberto <58601124+YokoCross@users.noreply.github.com>
2021-02-02 16:35:49 -0300

    first commit

---
+++ app/src/main/java/com/project/consultcep/ui/home/HomeViewModel.kt
@@ -0,0 +1,18 @@
+package com.project.consultcep.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+
+    fun getValue(){
+
+        _text.postValue("Oloko bixo")
+    }
+}
