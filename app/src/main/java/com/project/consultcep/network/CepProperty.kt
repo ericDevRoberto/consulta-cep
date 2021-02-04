7d2cd0f8b4ba61bec30afad0d46867c3443b1226
Eric Roberto <58601124+YokoCross@users.noreply.github.com>
2021-02-04 15:00:34 -0300

    consult layout added

--- app/src/main/java/com/project/consultcep/network/CepProperty.kt
+++ app/src/main/java/com/project/consultcep/network/CepProperty.kt
@@ -2,9 +2,9 @@ package com.project.consultcep.network
 
 data class CepProperty(
     val status: Int,
-    val code: String = String(),
-    val state: String = String(),
-    val city: String = String(),
+    //   val code: String = String(),
+    //  val state: String = String(),
+    //val city: String = String(),
     val district: String = String(),
     val address: String = String()
 )
